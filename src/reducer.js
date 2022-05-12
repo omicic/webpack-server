67d7226b547afec25fdc2097b7bdae86da8294a0
Olivera Micic <omicic@gmail.com>
2022-05-12 12:13:29 +0200

    Object->spread operator

--- src/reducer.js
+++ src/reducer.js
@@ -7,25 +7,32 @@ function reducer(state = initState, action) {
         case actions.START:
             return state;
         case actions.DISPLAY_ACCOUNTS_ACTION:
-            return Object.assign({}, state, {
+            return {...state, display: 1};
+           /*  return Object.assign({}, state, {
                 display: 1
-            });
+            }); */
         case actions.DISPLAY_ADD_ACCOUNT_ACTION:
-            return Object.assign({}, state, {
+            return {...state, display: 2};
+          /*   return Object.assign({}, state, {
                 display: 2
-            });
+            }); */
         case actions.ADD_NEW_ACCOUNTS_ACTION:
-            return Object.assign({}, state, {
+            return {...state, 
+            accounts: [...state.accounts, action.payload.newAccount]};
+
+          /*   return Object.assign({}, state, {
                 accounts: [...state.accounts, action.payload.newAccount]
-            })
+            }) */
         case actions.DELETE_ACCOUNT_ACTION:
             let filteredAccounts = state.accounts.filter(account => {
                 return account.id !== action.payload.id;
             })
 
-            return Object.assign({}, state, {
+            return {...state,accounts: filteredAccounts}
+
+          /*   return Object.assign({}, state, {
                 accounts: filteredAccounts
-            });
+            }); */
             
         default:
             return state;
